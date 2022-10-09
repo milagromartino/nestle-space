803f93b4b9e66ba689aa605fe4c936c00ffcd6c1
Milagro Martino <miliarami02@gmail.com>
2022-10-09 20:45:31 -0300

    Add user profile selector

--- components/Layout/UserCard.vue
+++ components/Layout/UserCard.vue
@@ -1,12 +1,40 @@
 <template>
-  <v-btn elevation="0" color="#f3f0e9" class="text-capitalize body-1 py-3">
-    <v-img
-      src="/profile-pic.png"
-      class="rounded-circle"
-      width="40"
-      height="40"
-    />
-    <p class="mb-0 mx-3">Rodrigo Orella</p>
-    <v-icon large> mdi-chevron-down </v-icon>
-  </v-btn>
+  <v-select
+    v-model="selectedUser"
+    :items="users"
+    item-text="name"
+    item-value="id"
+    solo
+    flat
+    filled
+    style="width: 250px"
+    background-color="#f3f0e9"
+    class="mt-7 elevation-0 rounded-0 font-weight-medium"
+  >
+    <template #prepend-inner>
+      <v-img :src="selectedUserProfile" width="40" contain></v-img> </template
+    >/>
+  </v-select>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data() {
+    return {
+      selectedUserProfile: '/profile-pic.png',
+      selectedUser: 'Rodrigo Orella',
+      users: [
+        {
+          name: 'Rodrigo Orella',
+          icon: '/profile-pic.png',
+        },
+        {
+          name: 'Carlota Carlota',
+          icon: '/profile-pic.png',
+        },
+      ],
+    }
+  },
+})
+</script>
